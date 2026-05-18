14a6c0e5cba006466fed995b57a0aa5efddcaaa4
FernandoQu <fernando.quezada@udla.edu.ec>
2026-05-18 17:33:33 -0500

    Se agrego datos.c y .h

---
+++ datos.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int funRecursosDisponibles(int recursos){
+    printf("");
+}
+
+int funTiempoDeFabricacion(int tiempoDeFabricacion){
+    printf("");
+}
---
+++ datos.h
@@ -0,0 +1,6 @@
+#ifndef DATOS_H
+#define DATOS_H
+
+
+
+#endif

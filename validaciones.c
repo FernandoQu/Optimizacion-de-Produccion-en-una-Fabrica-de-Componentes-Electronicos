709fdb622dc1b42d71c561383c623e64a6484a50
FernandoQu <fernando.quezada@udla.edu.ec>
2026-05-18 19:20:01 -0500

    Se agrego la funcion para buscar por indice

--- validaciones.c
+++ validaciones.c
@@ -41,7 +41,7 @@ int esSoloLetras(char cadena[]) {
     return 1;
 }
 
-int existeProducto(char nombreBuscado[], char matrizNombres[][50]) {
+int existeProducto(char nombreBuscado[], char matrizNombres[][15]) {
     for (int i = 0; i < 5; i++) {
         int sonIguales = 1;
         int j = 0;
@@ -70,4 +70,25 @@ int validarLongitud(char cadena[]) {
         return 0;
     }
     return 1;
+}
+
+int buscarIndiceProducto(char nombreBuscado[], char matrizNombres[][50], int cantidades[]) {
+    for (int i = 0; i < 5; i++) {
+        // Solo buscamos en los espacios que sí tienen un producto (cantidad > 0)
+        if (cantidades[i] > 0) {
+            int sonIguales = 1;
+            int j = 0;
+            while (nombreBuscado[j] != '\0' || matrizNombres[i][j] != '\0') {
+                if (nombreBuscado[j] != matrizNombres[i][j]) {
+                    sonIguales = 0;
+                    break;
+                }
+                j++;
+            }
+            if (sonIguales == 1) {
+                return i; // ¡Lo encontramos! Devolvemos la posición
+            }
+        }
+    }
+    return -1; // No se encontró en ningún lado
 }
--- validaciones.h
+++ validaciones.h
@@ -3,8 +3,9 @@
 
 int pedirEnteroValido();
 int esSoloLetras(char cadena[]);
-int existeProducto(char nombreBuscado[], char matrizNombres[][50]);
+int existeProducto(char nombreBuscado[], char matrizNombres[][15]);
 int buscarEspacioLibre(int cantidades[]);
 int validarLongitud(char cadena[]);
+int buscarIndiceProducto(char nombreBuscado[], char matrizNombres[][50], int cantidades[]);
 
 #endif
